d0019d7d2a55c905e54a46a771512731fc7602a5
ronwellman <ron.wellman01@gmail.com>
2018-01-22 22:43:55 -0500

    Updated the output display

--- wunderground.py
+++ wunderground.py
@@ -12,4 +12,29 @@ url = 'http://api.wunderground.com/api/{}/{}/q/{}/{}.json'.format(config.api_key
 r = requests.get(url)
 
 if r.status_code == 200:
-    print('It feels like {}F out there!'.format(r.json()['current_observation']['feelslike_f']))
+    curr_obs = r.json()['current_observation']
+    city = curr_obs['observation_location']['city'].split(',')[-1].strip()
+    state = curr_obs['observation_location']['state']
+    feelslike_f = curr_obs['feelslike_f']
+    temp_f = curr_obs['temp_f']
+    observation_time_rfc822 = curr_obs['observation_time_rfc822'][:-6]
+    weather = curr_obs['weather']
+    wind_mph = curr_obs['wind_mph']
+    wind_dir = curr_obs['wind_dir']
+    
+    output = ['{:<13}{}, {}'.format('Location:',city,state)]
+    output.append('{:<13}{}'.format('Time:',observation_time_rfc822))
+    output.append('{:<13}{}'.format('Conditions:',weather))
+    output.append('{:<13}{} F'.format('Temp:',temp_f))
+    output.append('{:<13}{} @ {} MPH'.format('Winds:',wind_dir,wind_mph))
+
+    width = max((len(l) for l in output)) + 4
+    print('*' * width)
+    for line in output:
+        spaces = width - (len(line) + 3) 
+        print('| {}'.format(line) + ' ' * spaces + '|')
+    print('*' * width)
+else:
+    print('Failed to retrieve current weather')
+
+
