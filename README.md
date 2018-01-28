ce9acf24ade2d6a97f52e308e8d27775146be6ec
ronwellman <ron.wellman01@gmail.com>
2018-01-27 23:46:29 -0500

    Cleaned up codebase with pylint and improved module documentation

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # wunderground
-Currently displays the most recent weather observation for Augusta, GA
+Currently displays the most recent weather observation using the Weather Underground API
 
 ### Setup
+** Not really needed but here just in case **
 * `python3 -m venv venv`
 * `source ./venv/bin/activate`
 * `pip3 install requests`
@@ -9,7 +10,7 @@ Currently displays the most recent weather observation for Augusta, GA
 * `deactivate`
 
 ### Todo
-1. Add options for custom city and state
+1. <complete> Add options for custom city and state
 1. Add support for querying additional feature sets
 1. Add support for triggering actions based on temperature
 
--- wunderground.py
+++ wunderground.py
@@ -1,54 +1,95 @@
 #!/usr/bin/env python3
+'''
+    wunderground
 
-import sys, requests, argparse
+    This CLI app utilizes the Weather Underground API to query
+    basic weather conditions for a given city/state or zip code.
+
+    Dependencies:
+        requests, argparse
+
+    Required Command Line Arguments:
+        -c City
+        -s State
+        -z Zipcode
+
+    API key:
+        imported from separate config file
+'''
+import sys
+import argparse
+import requests
 import config
+
+def parse_json(req):
+    '''
+    parse_json(requests) -> list
+
+    parse_json takes in a requests object and extracts location, time,
+    temperature, and winds information and returns a list containing
+    that information preformated for display
+    '''
+    curr_obs = req.json()['current_observation']
+    city = curr_obs['observation_location']['city'].split(',')[-1].strip()
+    state = curr_obs['observation_location']['state']
+    temp_f = curr_obs['temp_f']
+    observation_time_rfc822 = curr_obs['observation_time_rfc822'][:-6]
+    weather = curr_obs['weather']
+    wind_mph = curr_obs['wind_mph']
+    wind_dir = curr_obs['wind_dir']
+
+    output = ['{:<13}{}, {}'.format('Location:', city, state)]
+    output.append('{:<13}{}'.format('Time:', observation_time_rfc822))
+    output.append('{:<13}{}'.format('Conditions:', weather))
+    output.append('{:<13}{} F'.format('Temp:', temp_f))
+    output.append('{:<13}{} @ {} MPH'.format('Winds:', wind_dir, wind_mph))
+
+    return output
+
+
 
 
 def main():
-    parser = argparse.ArgumentParser(description='CLI app for looking up the weather given City/State or Zip')
-    parser.add_argument('-c', dest='city',help='City to lookup')
-    parser.add_argument('-s', dest='state',help='State to lookup')
-    parser.add_argument('-z', dest='zipcode',help='Zipcode to lookup')
+    '''
+    main() -> int
+
+    Utilizes the requests module to query the Weather Underground http
+    API and parses through the resulting JSON to extract location
+    specific weather information.
+    '''
+    parser = argparse.ArgumentParser(description='CLI app for looking up the weather'\
+            ' given City/State or Zip')
+    parser.add_argument('-c', dest='city', help='City to lookup')
+    parser.add_argument('-s', dest='state', help='State to lookup')
+    parser.add_argument('-z', dest='zipcode', help='Zipcode to lookup')
     args = parser.parse_args()
 
     city = args.city
     state = args.state
     zipcode = args.zipcode
     feature = 'conditions'
-    
+
     if zipcode:
-        url = 'http://api.wunderground.com/api/{}/{}/q/{}.json'.format(config.api_key,feature,zipcode)
+        url = 'http://api.wunderground.com/api/{}/{}/q/{}.json'.format(\
+                config.api_key, feature, zipcode)
     else:
-        url = 'http://api.wunderground.com/api/{}/{}/q/{}/{}.json'.format(config.api_key,feature,state,city)
+        url = 'http://api.wunderground.com/api/{}/{}/q/{}/{}.json'.format(\
+                config.api_key, feature, state, city)
 
-    r = requests.get(url)
+    req = requests.get(url)
 
-    if r.status_code == 200:
-        curr_obs = r.json()['current_observation']
-        city = curr_obs['observation_location']['city'].split(',')[-1].strip()
-        state = curr_obs['observation_location']['state']
-        feelslike_f = curr_obs['feelslike_f']
-        temp_f = curr_obs['temp_f']
-        observation_time_rfc822 = curr_obs['observation_time_rfc822'][:-6]
-        weather = curr_obs['weather']
-        wind_mph = curr_obs['wind_mph']
-        wind_dir = curr_obs['wind_dir']
-    
-        output = ['{:<13}{}, {}'.format('Location:',city,state)]
-        output.append('{:<13}{}'.format('Time:',observation_time_rfc822))
-        output.append('{:<13}{}'.format('Conditions:',weather))
-        output.append('{:<13}{} F'.format('Temp:',temp_f))
-        output.append('{:<13}{} @ {} MPH'.format('Winds:',wind_dir,wind_mph))
-
+    if req.status_code == 200:
+        output = parse_json(req.json())
         width = max((len(l) for l in output)) + 4
         print('*' * width)
         for line in output:
-            spaces = width - (len(line) + 3) 
+            spaces = width - (len(line) + 3)
             print('| {}'.format(line) + ' ' * spaces + '|')
         print('*' * width)
     else:
         print('Failed to retrieve current weather')
 
+    return 0
+
 if __name__ == "__main__":
     sys.exit(main())
-
